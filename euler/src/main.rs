9e69a76cff840ea3ebc5e67130d54a267b3bee95
shuban-789 <cybergeek11929@gmail.com>
2024-12-13 23:21:37 -0800

    decimal accounting

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -1,6 +1,6 @@
 fn d(x: f32, y: f32) -> f32 {
     let diffeq = (2.0 * x) + y;
-    return diffeq
+    return diffeq;
 }
 
 fn main() {
@@ -8,16 +8,16 @@ fn main() {
     let y_coord_init: f32 = 1.0;
     let delta_x: f32 = 0.2;
     let x_coord_final: f32 = 2.0;
-    
+
     let mut x_coord = x_coord_init;
     let mut y_coord = y_coord_init;
 
     while x_coord < x_coord_final {
-        println!("({}, {})", x_coord, y_coord);
+        println!("({:.001}, {})", x_coord as f32, y_coord as f32);
         let delta_y = d(x_coord, y_coord) * delta_x;
         y_coord += delta_y;
         x_coord += delta_x;
     }
 
-    println!("({}, {})", x_coord_final, y_coord);
+    println!("({:.001}, {})", x_coord_final as f32, y_coord as f32);
 }
