7162e236d59d67200d8de550f7bae96c3dd59581
shuban-789 <cybergeek11929@gmail.com>
2025-03-15 01:12:05 -0700

    finalized working euler's method

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -13,11 +13,18 @@ fn main() {
     let mut y_coord = y_coord_init;
 
     while x_coord < x_coord_final {
-        println!("({:.001}, {})", x_coord as f32, y_coord as f32);
+        println!("=====================");
+        println!("Point: ({:.001}, {})", x_coord as f32, y_coord as f32);
+        println!("Delta x: {}", delta_x);
+        println!("dy/dx @ ({:.001}, {}): {}", x_coord as f32, y_coord as f32, d(x_coord, y_coord));
         let delta_y = d(x_coord, y_coord) * delta_x;
+        println!("Delta y: {}", delta_y);
         y_coord += delta_y;
         x_coord += delta_x;
     }
-
-    println!("({:.001}, {})", x_coord_final as f32, y_coord as f32);
+    println!("=====================");
+    println!();
+    println!("Final Point: ({:.001}, {})", x_coord_final as f32, y_coord as f32);
+    println!();
+    println!("y is approximately equal to {} for x is equal to {}", y_coord as f32, x_coord_final as f32);
 }
